5e01c4b4ba1dfe3383a73fa0698ca46c0817b97e
Tobias Bocanegra <tripod@adobe.com>
2022-07-08 14:12:45 +0200

    feat: improve message

--- src/index.js
+++ src/index.js
@@ -278,7 +278,7 @@ async function testReadAccessOnedrive(ctx, info, client) {
   } catch (e) {
     log.warn('unable to resolve sharelink', e);
     if (e.details.code === 'accessDenied') {
-      return `Not authorized to access underlying data source. Please make sure that the enterprise application: "Helix Service (${client.auth.clientId})" is consented for the required scopes.`;
+      return `The resource specified in the fstab.yaml does either not exist, or you do not have permission to access it. Please make sure that the url is correct, the enterprise application: "Helix Service (${client.auth.clientId})" is consented for the required scopes, and that the logged in user has permissions to access it.`;
     } else {
       return `Unable to validate access: ${e.message}`;
     }
